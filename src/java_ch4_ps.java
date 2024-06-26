2c5c79d7a0ea7893cfc8005e1f6a45f7132d245c
harshkapadia2805 <hrk2805@gmail.com>
2024-06-27 00:02:05 +0530

    26_June

---
+++ src/java_ch4_ps.java
@@ -0,0 +1,80 @@
+import java.util.Scanner;
+
+public class java_ch4_ps {
+    public static void main(String[] args) {
+
+//        Question2
+
+//        byte m1, m2, m3;
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter your marks in Physics : ");
+//        m1 = sc.nextByte();
+//        System.out.println("Enter your marks in Chemistry : ");
+//        m2 = sc.nextByte();
+//        System.out.println("Enter your marks in Maths : ");
+//        m3 = sc.nextByte();
+//
+//        float avg = (m1+m2+m3)/3.0f;
+//        System.out.println("Percentage : "+avg);
+//        if(avg>=40 && m1>=33 && m2>=33 && m3>=33){
+//            System.out.println("Promoted");
+//        }
+//        else{
+//            System.out.println("Sorry");
+//        }
+
+//        Question3
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter your income in lakhs : ");
+//        float tax = 0;
+//        float income = sc.nextFloat();
+//        if(income <= 2.5){
+//            tax = tax + 0;
+//        }
+//        else if(income > 2.5f && income <= 5f){
+//            tax = tax + 0.05f * (income - 2.5f);
+//        }
+//        else if(income > 5f && income <= 10f){
+//            tax = tax + 0.05f * (5.0f - 2.5f);
+//            tax = tax + 0.2f * (income - 5f);
+//        }
+//        else if(income > 10f){
+//            tax = tax + 0.05f * (5.0f - 2.5f);
+//            tax = tax + 0.2f * (10.0f - 5.0f);
+//            tax = tax + 0.3f * (income - 10.0f);
+//        }
+//        System.out.println("Total tax paid : "+ tax);
+
+
+//        Question4
+
+//        Scanner sc = new Scanner(System.in);
+//        int day = sc.nextInt();
+////        int day = 2;
+//        switch (day){
+//            case 1 -> System.out.println("Monday");
+//            case 2 -> System.out.println("Tuesday");
+//            case 3 -> System.out.println("Wednesday");
+//            case 4 -> System.out.println("Thursday");
+//            case 5 -> System.out.println("Friday");
+//            case 6 -> System.out.println("Saturday");
+//            case 7 -> System.out.println("Sunday");
+//        }
+
+
+//        Question6
+
+//        Scanner sc = new Scanner(System.in);
+//        String website = sc.next();
+//        if(website.endsWith(".org")){
+//            System.out.println("Organizational Website");
+//        }
+//        else if (website.endsWith(".com")) {
+//            System.out.println("Commercial Website");
+//        }
+//        else if (website.endsWith(".in")){
+//            System.out.println("Indian Website");
+//        }
+    }
+}
