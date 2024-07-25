6e6efd86e6253fe8bde3fe91020c411c2dfe63ad
harshkapadia2805 <hrk2805@gmail.com>
2024-07-25 16:33:10 +0530

    ch12

---
+++ ex5/Square.java
@@ -0,0 +1,12 @@
+package com.harsh.shape;
+public class Square extends Shape {
+    
+    Square(int dim1){
+        super(dim1, -1);
+    }
+
+    public int area(){
+        return this.dim1 * this.dim1;
+    }
+}
+
