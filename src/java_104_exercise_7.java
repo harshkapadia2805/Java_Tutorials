3ab694ee9395f5340da1d9cc3d8d460af660b5b4
harshkapadia2805 <hrk2805@gmail.com>
2024-08-06 18:16:59 +0530

    library

---
+++ src/java_104_exercise_7.java
@@ -0,0 +1,14 @@
+public class java_104_exercise_7 {
+    public static void main(String[] args) {
+        /*
+        Library management system which is capable of issueing books to students
+        Book info
+        1. Book name
+        2. Book author
+        3. issued to
+        4. issued on
+        User should be able to add books, return issued books, issue books
+        Assume that all the users are registered with their names in the central database
+         */
+    }
+}
